1399c119696032afda56fe22bc1fb84b86cdac0d
JULIANO ROHDE <julianorohde@hotmail.com>
2023-03-02 19:06:37 -0400

    Add tests in Hero model

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,6 @@
 class Hero < ApplicationRecord
-  validates :name, :token, presence: true
-  validates :name, uniqueness: { scope: :token, case_sensitive: false }
+  validates :name, presence: true, uniqueness: { scope: :token, case_sensitive: false }
+  validates :name, :token, presence: true, length: { minimum: 5 }
 
   scope :by_token, ->(token) { where(token: token) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
