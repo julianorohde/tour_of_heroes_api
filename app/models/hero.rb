27e11f342a4c369aaa6bfee32376b0e77215eec9
JULIANO ROHDE <julianorohde@hotmail.com>
2023-03-03 16:13:59 -0400

    Change hero search by name

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -3,6 +3,6 @@ class Hero < ApplicationRecord
   validates :name, :token, presence: true, length: { minimum: 5 }
 
   scope :by_token, ->(token) { where(token: token) }
-  scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
+  scope :search, ->(name) { where('LOWER(name) LIKE ?', "%#{name.downcase}%") if name.present? }
   scope :sorted_by_name, -> { order(:name) }
 end
